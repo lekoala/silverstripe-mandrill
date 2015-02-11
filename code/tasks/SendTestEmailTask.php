8fe558f35d9d862bc00b061102f4d8b0df17e47d
Thomas Portelange <thomas.portelange@boomerang.be>
2015-02-11 17:25:57 +0100

    test email task

---
+++ code/tasks/SendTestEmailTask.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * A simple task to test if your emails are sending properly
+
+ * @author LeKoala
+ */
+class SendTestEmailTask extends BuildTask
+{
+    protected $description = 'Send a sample email to admin or to ?email=';
+
+    function run($request)
+    {
+        $config = SiteConfig::current_site_config();
+        $email  = new MandrillEmail();
+
+        $member = Member::currentUser();
+        $to     = $request->getVar('email');
+
+        $email->setSampleContent();
+
+        if (!$to) {
+            $email->setToMember($member);
+        } else {
+            $email->setTo($to);
+        }
+
+        $email->setSubject('Sample email from '.$config->Title);
+
+        echo 'Sending to '.htmlentities($email->To()).'<br/>';
+        echo 'Using theme : '.$email->getTheme().'<br/>';
+
+        $res = $email->send();
+
+        foreach($res as $v) {
+            if(!is_string($v)) {
+                echo "<pre>";
+                var_dump($v);
+                echo "</pre>";
+            }
+            else {
+                echo $v . '<br/>';
+            }
+        }
+    }
+}
