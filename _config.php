a06ea5069d9b4426372daddb5d1d55e11539a548
Thomas Portelange <thomas@lekoala.be>
2019-08-23 12:13:33 +0200

    php 7.2 compat

--- _config.php
+++ _config.php
@@ -1,4 +1,6 @@
 <?php
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 // Autosetup if key is defined in _ss_environment or yml
 if (defined('MANDRILL_API_KEY') && MANDRILL_API_KEY !== '' || Config::inst()->get('MandrillMailer', 'mandrill_api_key')) {
 	MandrillMailer::setAsMailer();
